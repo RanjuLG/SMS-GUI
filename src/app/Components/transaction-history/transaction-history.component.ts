c4b78885aa8fe16a63bd906f2f91cdcd99b4fd37
Ranju <141168492+RanjuLG@users.noreply.github.com>
2024-08-05 20:22:12 +0530

    bug fixed

--- src/app/Components/transaction-history/transaction-history.component.ts
+++ src/app/Components/transaction-history/transaction-history.component.ts
@@ -30,6 +30,7 @@ export class TransactionHistoryComponent implements OnInit {
   transactionsPerPage: number = 10;
   transactionsPerPageOptions: number[] = [1, 5, 10, 15, 20];
   searchControl = new FormControl();
+  transactionIds?: number[];
 
   constructor(
     private modalService: NgbModal, 
@@ -96,6 +97,9 @@ export class TransactionHistoryComponent implements OnInit {
 
   deleteSelectedTransactions() {
     const selectedTransactionIds = this.transactions.filter(transaction => transaction.selected).map(transaction => transaction.transactionId);
+    this.transactionIds?.concat(selectedTransactionIds)
+    console.log("this.transactionIds?",this.transactionIds);
+    console.log("selectedTransactionIds",selectedTransactionIds)
     if (selectedTransactionIds.length === 0) {
       Swal.fire('No transactions selected', 'Please select at least one transaction to delete.', 'warning');
       return;
@@ -125,6 +129,8 @@ export class TransactionHistoryComponent implements OnInit {
 
   deleteTransaction(index: number) {
     const transactionId = this.transactions[index].transactionId;
+    console.log("transactionId", transactionId);
+  
     Swal.fire({
       title: 'Delete Transaction',
       text: 'Are you sure you want to delete this transaction?',
@@ -135,7 +141,11 @@ export class TransactionHistoryComponent implements OnInit {
       cancelButtonText: 'Cancel'
     }).then((result) => {
       if (result.isConfirmed) {
-        this.apiService.deleteTransaction(transactionId).subscribe(() => {
+        // Ensure transactionIds is initialized and add transactionId to it
+        this.transactionIds = this.transactionIds || [];
+        this.transactionIds.concat(transactionId);
+  
+        this.apiService.deleteMultipleTransactions(this.transactionIds).subscribe(() => {
           this.loadTransactions();
           Swal.fire('Deleted!', 'Transaction has been deleted.', 'success');
         }, error => {
@@ -147,6 +157,7 @@ export class TransactionHistoryComponent implements OnInit {
       }
     });
   }
+  
 
   toggleAllSelections(event: Event) {
     const checked = (event.target as HTMLInputElement).checked;
