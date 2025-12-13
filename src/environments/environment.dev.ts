5d05ebc9e1328f9451f92b56888c263e1e10e0be
Ranju <141168492+RanjuLG@users.noreply.github.com>
2025-12-14 02:55:52 +0530

    Add environment configs and update build scripts

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://localhost:7217',
+  appName: 'SMS - Development',
+  enableDebugTools: true,
+  logLevel: 'debug'
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://sms-api-ecercnh3bmdtc9cz.southeastasia-01.azurewebsites.net',
+  appName: 'SMS - Production',
+  enableDebugTools: false,
+  logLevel: 'error'
+};
