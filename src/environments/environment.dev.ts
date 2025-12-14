3d6503c3128700cd16f3fa4787675912dcf03bc3
Ranju <141168492+RanjuLG@users.noreply.github.com>
2025-12-14 21:30:45 +0530

    Update API URL in environment configs
    .

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7217',
+  apiUrl: 'https://sms-api-ecercnh3bmdtc9cz.southeastasia-01.azurewebsites.net',
   appName: 'SMS - Development',
   enableDebugTools: true,
   logLevel: 'debug'
